398734bca981a7bfa25bd62f7859cc067460966f
Tigabu <tigabu.m@productera.io>
2024-08-07 22:19:50 +0300

    feat: added redux and router config

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <Provider store={configureStore()}>
     <App />
+    </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
