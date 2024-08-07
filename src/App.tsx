5b3e2861f7b420b1f5fcd102a8da90465e5368fa
Tigabu <tigabu.m@productera.io>
2024-08-07 17:01:27 +0300

    feat: added css grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navigation">Nav</div>
+      <div className="sidebar">sidebar</div>
+      <div className="leftmenu">leftmenu</div>
+      <div className="content">content</div>
+      <div className="rightmenu">rightmenu</div>
     </div>
   );
 }
