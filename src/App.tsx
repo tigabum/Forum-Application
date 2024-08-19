2c092a8fe263f4f4b8cb6f3b7122a2a50dcdc3bd
Tigabu <tigabu.m@productera.io>
2024-08-19 15:25:01 +0300

    feat: added thread component

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 import Home from "./components/routes/Home";
+import Thread from "./components/routes/thread/Thread";
 
 function App() {
   const renderHome = (props: any) => <Home {...props} />;
+  const renderThread = (props:any) => <Thread/>
 
   return (
     <Switch>
       <Route exact={true} path="/" render={renderHome} />
       <Route path="/categorythreads/:categoryId" render={renderHome} />
+      <Route path="/thread/:id" render={renderThread} />
+
+      
 
       {/* <Nav />
       <Main />
---
+++ src/components/routes/thread/Thread.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Thread=()=> {
+  return (
+    <div>
+      Thread component
+    </div>
+  )
+}
+
+export default Thread
